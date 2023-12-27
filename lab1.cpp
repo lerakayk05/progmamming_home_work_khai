a60fc4d5557a41e0ff9c87526e057a472d2581c3
lerakayk05 <154271754+lerakayk05@users.noreply.github.com>
2023-12-27 22:07:32 +0200

    Update lab1.cpp

--- lab1.cpp
+++ lab1.cpp
@@ -1,123 +1,30 @@
 #include <iostream>
-#include <locale.h>
-#include <cmath>
-
+							//Каюк Валерія 15 24
 using namespace std;
 
-//Каюк Валерія 14 30
-
-void if14();
-void geo30();
-void geo30s();
-
-
 int main() {
-	int choice = 0; //Декларування змінної задля choice
-	cout << "Choice the task: " << endl <<
-		"1. If 14" << endl <<
-		"2. Geo 15" << endl <<
-		"3. Geo 15(Square and perimeter)" << endl;
-
-	cin >> choice;
-
-	switch (choice)
-	{
-	case 1:
-	{
-		// if 14
-		if14();
-		break;
-	}
-	case 2:
-	{
-		// geo30(Належність точки до фігури)
-		geo30();
-		break;
-	}
-	case 3:
-	{
-		//geo15.2(Периметр та площа)
-		geo30s();
-		break;
-	}
-	default:
-	{
-		cout << "Wrong variant\n";
-		break;
-	}
-	}
-}
-
-void if14()
-{
-	// IF 14
-	cout << " IF 14" << endl;
-
-	float A, B, C;
-	cout << "Enter A, B and C: " << endl;
-	cin >> A >> B >> C;
-
-	// Перевірка на мінімальне число
-	float minimum = A;
-	if (B < minimum) {
-		minimum = B;
-	}
-	if (C < minimum) {
-		minimum = C;
-	}
-
-	//Перевірка на максимальне число
-	float maximum = A;
-	if (B > maximum) {
-		maximum = B;
-	}
-	if (C > maximum) {
-		maximum = C;
-	}
-
-	cout << "Lowest number is " << minimum << " and highiest number is " << maximum;
-
-}
-
-void geo30() {
-	cout << " GEO 30" << endl;
 	
-	int r = 0, x = 0, y = 0;				 //Декларування змінних
-	bool ar1, ar2;							 //Декларування змінних типу bool
+	int A, B, C;						//Begin15
 
-	cout << "Enter a radius:  ";
-	cin >> r;
+	cout << "Enter A, B and C: ";  //
+	cin >> A >> B >> C;    //Присвоєння даних змінним
 
-	cout << "Enter X and Y:  ";
-	cin >> x >> y;
+	int temp = C;
+	 
+	C = B;				 //Розрахунок
+	B = A;
+	A = temp;
 
-	ar1 = pow((x + r), 2) + pow(y, 2) > pow(r, 2) && y > 0 && y < r && x >-1 * r && x < 0;		// Розрахунок знаходження першої фігури
-	ar2 = pow((x + r), 2) + pow(y, 2) < pow(r, 2) && y > -x - 2 * r && y < 0 && x < -1 * r;			 // Розрахунок знаходження другої фігури
+	cout << "Edited A = " << A << " ,B = " << B << " ,C = " << C << endl;
 
-	if (ar1 || ar2) {
-		cout << "In region!" << endl;
-	}
-	else {
-		cout << "Out of region!";
-	}
-}
+	cout << "--------------------------------------------------------" << endl;
 
-void geo30s() {
-	int r, x, y;
-	const float pi = 3.14;
-	cout << "Enter a radius: ";
-	cin >> r;
+	float TC = 0;  // Begin 24
 
-	float perimeter1 = (2 * pi * r) / 4 + 2 * r;
-	float perimeter2 = r + r + r * sqrt(2);
-	
+	cout << "Enter a temperature(Celsius): ";
+	cin >> TC; //Присвоєння даних змінній
 
-	float square1 = pow(r, 2) - ((pi * pow(r, 2)) / 4);
-	float square2 = pow(r,2)/2;
+	float TF = (TC * 9.0 / 5) + 32; //Розрахунок
 
-	cout << "Square of first figure = " << square1 << endl; // Виведення результату
-	cout << "Square of second figure = " << square2 << endl; // Виведення результату
-
-	cout << "Perimeter of first figure = " << perimeter1 << endl; // Виведення результату
-	cout << "Perimeter of second figure = " << perimeter2 << endl; // Виведення результату
+	cout << "Temperature in Fahrenheit: " << TF;
 }
