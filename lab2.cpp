a8ddffeca5d03e7cb72b2db6226938eef64749f3
lerakayk05 <154271754+lerakayk05@users.noreply.github.com>
2023-12-27 22:04:30 +0200

    Update lab2.cpp

--- lab2.cpp
+++ lab2.cpp
@@ -1,123 +1,49 @@
 #include <iostream>
-#include <locale.h>
 #include <cmath>
 
 using namespace std;
 
-//Каюк Валерія 14 30
-
-void if14();
-void geo30();
-void geo30s();
-
+					// Каюк Валерія 18 32 39
 
 int main() {
-	int choice = 0; //Декларування змінної задля choice
-	cout << "Choice the task: " << endl <<
-		"1. If 14" << endl <<
-		"2. Geo 15" << endl <<
-		"3. Geo 15(Square and perimeter)" << endl;
-
-	cin >> choice;
-
-	switch (choice)
-	{
-	case 1:
-	{
-		// if 14
-		if14();
-		break;
-	}
-	case 2:
-	{
-		// geo30(Належність точки до фігури)
-		geo30();
-		break;
-	}
-	case 3:
-	{
-		//geo15.2(Периметр та площа)
-		geo30s();
-		break;
-	}
-	default:
-	{
-		cout << "Wrong variant\n";
-		break;
-	}
-	}
-}
-
-void if14()
-{
-	// IF 14
-	cout << " IF 14" << endl;
-
-	float A, B, C;
-	cout << "Enter A, B and C: " << endl;
-	cin >> A >> B >> C;
-
-	// Перевірка на мінімальне число
-	float minimum = A;
-	if (B < minimum) {
-		minimum = B;
-	}
-	if (C < minimum) {
-		minimum = C;
-	}
-
-	//Перевірка на максимальне число
-	float maximum = A;
-	if (B > maximum) {
-		maximum = B;
-	}
-	if (C > maximum) {
-		maximum = C;
-	}
-
-	cout << "Lowest number is " << minimum << " and highiest number is " << maximum;
-
-}
-
-void geo30() {
-	cout << " GEO 30" << endl;
+	// Integer 18
+	cout << " INTEGER 18" << endl;
+	int A = 0;                                    //Декларація змінної
+	cout << "Enter A(>1000): " << endl;
+	cin >> A;
 	
-	int r = 0, x = 0, y = 0;				 //Декларування змінних
-	bool ar1, ar2;							 //Декларування змінних типу bool
+	int thousands = A / 1000;
+	cout << "Thousands in number " << A << " is " << thousands << endl;
 
-	cout << "Enter a radius:  ";
-	cin >> r;
+	cout << "---------------------------------------------------" << endl;
 
-	cout << "Enter X and Y:  ";
-	cin >> x >> y;
+	// Boolean 32
+		cout << " BOOLEAN 32" << endl;
+		int A1, B1, C1;							//Декларація змінних
+		bool IsRightTriangle;
+		cout << "Enter A, B and C: " << endl;
+		cin >> A1 >> B1 >> C1;
 
-	ar1 = pow((x + r), 2) + pow(y, 2) > pow(r, 2) && y > 0 && y < r && x >-1 * r && x < 0;		// Розрахунок знаходження першої фігури
-	ar2 = pow((x + r), 2) + pow(y, 2) < pow(r, 2) && y > -x - 2 * r && y < 0 && x < -1 * r;			 // Розрахунок знаходження другої фігури
+		IsRightTriangle = (A1 * A1 + B1 * B1 == C1 * C1 || B1 * B1 + C1 * C1 == A1 * A1 || A1 * A1 + C1 * C1 == B1 * B1);
 
-	if (ar1 || ar2) {
-		cout << "In region!" << endl;
-	}
-	else {
-		cout << "Out of region!";
-	}
-}
+		cout << "This triangle is right?: " << boolalpha << IsRightTriangle << endl;
 
-void geo30s() {
-	int r, x, y;
-	const float pi = 3.14;
-	cout << "Enter a radius: ";
-	cin >> r;
+		cout << "---------------------------------------------------" << endl;
+	// Solve 39
+		cout << " SOLVE 39" << endl;
+		float x = 0;							//Декларація змінних
+		const float pi = 3.14;					//Декларація сталої П=3.14
 
-	float perimeter1 = (2 * pi * r) / 4 + 2 * r;
-	float perimeter2 = r + r + r * sqrt(2);
-	
+		cout << " Enter X: ";
+		cin >> x;
+		float xforsin = x * (180 / pi);				//Перевод радіан у градуси
 
-	float square1 = pow(r, 2) - ((pi * pow(r, 2)) / 4);
-	float square2 = pow(r,2)/2;
+		float square = pow((xforsin + 64), 2);		//Спрощення
+		float loghariphm = log(fabs(x)) / log(3);	//Спрощення	
 
-	cout << "Square of first figure = " << square1 << endl; // Виведення результату
-	cout << "Square of second figure = " << square2 << endl; // Виведення результату
+		float nom = pow(5, pow(x, 2) * pow(sin(xforsin), 2)) * pow(fabs(pow(sin(square), 2)) * loghariphm, 0.25);		// Числівник
+		float denom = pow(tan(xforsin), 3) * pow(sin(xforsin*xforsin*xforsin), 2);										// Знаменник
+		float y = nom / denom;			//Розрахунок
 
-	cout << "Perimeter of first figure = " << perimeter1 << endl; // Виведення результату
-	cout << "Perimeter of second figure = " << perimeter2 << endl; // Виведення результату
+		cout << "Y = " << y; // Виведення результату
 }
